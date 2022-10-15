35aa13ccbdd5a5dcd1644c1bf206c8565886f8a6
Jaz Kime <JazDKime@gmail.com>
2022-10-14 17:18:19 -0700

    made selected project change with hover of project icon

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import './resetStyles.css'
 import SideBar from './components/SideBar';
 import ProjectContainer from './components/spotlight-projects/ProjectContainer';
+import { Routes, Route } from 'react-router-dom'
+import Contact from './components/sidePages/Contact';
 
 function App() {
   return (
     <div className="App">
       <SideBar />
-      <ProjectContainer />
+
+    <Routes>
+      <Route path='/' element={<ProjectContainer />} />
+      <Route path='/contact-me' element={ <Contact />}/>
+    </Routes>
     </div>
   );
 }
---
+++ src/data/projects.js
@@ -0,0 +1,24 @@
+const projects = ([{
+    title: "Creation Station",
+    summary: "A site for character generation. Whether it be in devising inspiration for drawing or a story persona- from a robotic elf to a happy gnome with light magic, Creation Station is there to help in your artistic planning.",
+    stack: "CSS, JavaScript, React, Redux, Hooks",
+    img: null
+},
+{
+    title: "Journey Journal",
+    summary: "a personal journal site to remember your journey to where you are or where you wish to be.",
+    stack: "HTML, JavaScript, React, Hooks",
+    img: null
+}
+])
+
+export default projects;
+
+/**
+{
+    title: "",
+    summary: "",
+    stack: "",
+    img: ""
+}
+ */
